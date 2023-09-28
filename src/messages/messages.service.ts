ddd9bfed6fb97f40661f52f28c02212f6f4b9453
Lucas Sant'Anna <98597613+LSantAnnaB@users.noreply.github.com>
2023-09-28 16:44:55 -0300

    findAll() method

--- src/messages/messages.service.ts
+++ src/messages/messages.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class MessagesService {}
+export class MessagesService {
+  private messages = [
+    {
+      id:1,
+      text: "texto exemplo"
+    },
+    
+  ];
+  findAll(){
+    return this.messages;
+  }
+}
