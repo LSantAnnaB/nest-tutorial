72adab39d5eb2e534b71525937ea90cdd158e7fb
Lucas Sant'Anna <98597613+LSantAnnaB@users.noreply.github.com>
2023-09-28 16:47:22 -0300

    controller-findAll() method

--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
+import { MessagesService } from './messages.service';
 
 @Controller('messages')
-export class MessagesController {}
+export class MessagesController {
+
+  constructor( private MessagesService: MessagesService){}
+
+  @Get()
+  findAll(){
+    return this.MessagesService.findAll();
+  }
+ 
+}
